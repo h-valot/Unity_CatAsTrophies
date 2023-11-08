b37e7a5bc6a39c25941d43681460339ea7303842
Kamillekaz <70025826+Kamillekaz@users.noreply.github.com>
2023-11-08 17:07:43 +0100

    Added chocoMuffin

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -1,14 +1,31 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
 public class Enemy : Entity
 {
     public int enemyType;
 
+    [Header("REFERENCES")]
+    public GameObject graphicsParent;
+    public Animator animator;
+
+    [Header("GRAPHICS TWEAKING")]
+    public Vector3 battleRotation;
+    public Vector3 baseRotation;
+    public float battleScale;
+
+
+    // Place enemy on battlefield 
     public void Initialize(int _enemyType)
     {
         base.Initialize();
         enemyType = _enemyType;
-        
+
+        graphicsParent.transform.eulerAngles = battleRotation;
+        graphicsParent.transform.localScale *= battleScale;
+
         // GAME STATS
-        autoAttacks = Registry.entitiesConfig.enemies[enemyType].autoAttack;  
+        autoAttacks = Registry.entitiesConfig.enemies[enemyType].autoAttack;
     }
     
     private void OnEnable()
