05d2b2c469a456a543fe2997062b497e2dfcc437
nobukky <nobukky19@gmail.com>
2023-11-09 10:05:54 +0100

    settings in main menu scene & confimation exit enhanced

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -14,5 +14,7 @@ public class MainMenuManager : MonoBehaviour
             SceneManager.LoadScene("Init");
             return;
         }
+        
+        MenuManager.Instance.Initialize();
     }
 }
