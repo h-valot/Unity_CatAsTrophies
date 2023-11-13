f0fbec92f3dea2a3f91773a8478f5c2702cfc106
nobukky <nobukky19@gmail.com>
2023-11-13 12:08:42 +0100

    map generation

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -1,17 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public class MapManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MapManager Instance;
+    
+    public List<InterestPoint> interestPoints;
+
+    public int stageAmount = 7;
+    public int maxInterestPointPerStage = 5;
+    public int maxInterestPointFirstStage = 3;
+    public float spawnBattleChance, spawnShopChance, spawnEventChance, spawnMergeChance, spawnGraveyardChance, spawnCampfireChance;
+
+    private void Awake() => Instance = this;
+
+    public void Initialize()
+    {
+        interestPoints = new List<InterestPoint>();
+        GenerateMap();
+    }
+    
+    public void GenerateMap()
+    {
+        for (int stageIndex = 0; stageIndex < stageAmount; stageIndex++)
+        {
+            int interestPointPerStage;
+            if (stageIndex == 0) interestPointPerStage = maxInterestPointFirstStage;
+            else interestPointPerStage = maxInterestPointPerStage;
+            
+            for (int innerStageInterestPointIndex = 0; innerStageInterestPointIndex < interestPointPerStage; innerStageInterestPointIndex++)
+            {
+                interestPoints.Add(new InterestPoint(stageIndex));
+                
+                // based on a random gaussian number based itself on a seed, choose the interest point type 
+            }
+        }
+    }
+
+    public void SaveMapToAsset()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DeleteMap()
     {
         
     }
