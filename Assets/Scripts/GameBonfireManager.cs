278de41be9a51cc9bf2fc3837aa5ecd9db5eeb0c
nobukky <nobukky19@gmail.com>
2023-12-01 14:09:53 +0100

    scenes ui added

--- Assets/Scripts/GameBonfireManager.cs
+++ Assets/Scripts/GameBonfireManager.cs
@@ -1,19 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameBonfireManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
+        // load the init scene if it hasn't been loaded yet
         if (!Registry.isInitialized)
         {
             SceneManager.LoadScene("Init");
             return;
         }
+        
+        Registry.events.OnSceneLoaded?.Invoke();
     }
-
-
 }
