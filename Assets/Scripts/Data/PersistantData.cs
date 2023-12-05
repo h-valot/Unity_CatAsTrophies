8353b6acb43e9915d970e55cecc467dce5628b89
xionck <xarc35.game@gmail.com>
2023-12-05 16:44:50 +0100

    Music Added

--- Assets/Scripts/Data/PersistantData.cs
+++ Assets/Scripts/Data/PersistantData.cs
@@ -1,11 +1,15 @@
+using Unity.VisualScripting;
+
 namespace Data
 {
     [System.Serializable]
     public class PersistantData
     {
         public bool isSaved;
-        
+
         // current loaded and displayed map
         public Map map;
+
+        public float MusicVolumeValue = 0.5f;
     }
 }
